e57c87a985722fd5cf2fc0f51b0a9b82944161f4
Shogo Ogami <sg.tao.so@gmail.com>
2022-05-19 08:30:49 +0900

    update 01_vue_taskbox for add a new component

--- 01_vue_taskbox/.storybook/main.js
+++ 01_vue_taskbox/.storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   features: {
     postcss: false,
