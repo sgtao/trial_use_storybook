2862f4ceb795dd813869fe0907abdf654026725b
Shogo Ogami <sg.tao.so@gmail.com>
2022-05-20 06:54:37 +0900

    remove 01_vue_taskbox for try again

--- 01_vue_taskbox/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  transform: {
-    '^.+\\.vue$': 'vue-jest'
-  },
-  transformIgnorePatterns: ["/node_modules/(?!(@storybook/.*\\.vue$))"],
-}
