090aa8150261c927d1adda7d9154a90102245e15
Shogo Ogami <sg.tao.so@gmail.com>
2022-05-21 19:13:26 +0900

    update 01_trace for trace until 05.Screens

--- 01_trace_tutorial_vue/taskbox_vue/src/components/PureInboxScreen.stories.js
+++ 01_trace_tutorial_vue/taskbox_vue/src/components/PureInboxScreen.stories.js
@@ -1,4 +1,27 @@
+import { app } from '@storybook/vue3';
+import { fireEvent, within } from '@storybook/testing-library';
+import { createStore } from 'vuex';
+
 import PureInboxScreen from './PureInboxScreen.vue';
+
+import { action } from '@storybook/addon-actions';
+import * as TaskListStories from './PureTaskList.stories';
+
+const store = createStore({
+  state: {
+    tasks: TaskListStories.Default.args.tasks,
+  },
+  actions: {
+    pinTask(context, id) {
+      action("pin-task")(id);
+    },
+    archiveTask(context, id) {
+      action("archive-task")(id);
+    },
+  },
+});
+
+app.use(store);
 
 export default {
   component: PureInboxScreen,
@@ -19,3 +42,12 @@ export const Default = Template.bind({});
 
 export const Error = Template.bind({});
 Error.args = { error: true };
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
